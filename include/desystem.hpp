3a79859641d1f8e37e6671705e3cdb2f7c2a5a18
deam0nd <adrianomourao@protonmail.com>
2018-03-07 18:43:38 -0300

    Adding library scratch

---
+++ include/desystem.hpp
@@ -0,0 +1,54 @@
+/*
+ =========================================================================
+ This file is part of clDES
+
+ clDES: an OpenCL library for Discrete Event Systems computing.
+
+ clDES is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ clDES is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with clDES.  If not, see <http://www.gnu.org/licenses/>.
+
+ Copyright (c) 2018 - Adriano Mourao <adrianomourao@protonmail.com>
+                      madc0ww @ [https://github.com/madc0ww]
+
+ LacSED - Laboratório de Sistemas a Eventos Discretos
+ Universidade Federal de Minas Gerais
+
+ File: desystem.hpp
+ Description: DESystem class definition. DESystem is a graph, which is
+ modeled as a Sparce Adjacency Matrix.
+ =========================================================================
+*/
+
+#ifndef DESYSTEM_HPP
+#define DESYSTEM_HPP
+
+#include "viennacl/compressed_matrix.hpp"
+// #include <boost/numeric/ublas/matrix_sparse.hpp>
+
+namespace clDES {
+
+typedef float ScalarType;
+
+class DESystem {
+public:
+    DESystem();
+    virtual ~DESystem();
+    viennacl::compressed_matrix<ScalarType> &get_graph() const;
+
+private:
+    viennacl::compressed_matrix<ScalarType> *graph;
+};
+
+}  // namespace clDES
+
+#endif  // DESYSTEM_HPP
