e2754beb93524f7a9ed2376299dee02ec00f89cd
deam0nd <adrianomourao@protonmail.com>
2018-03-20 19:43:29 -0300

    Documenting transition_proxy

--- include/des/transition_proxy.hpp
+++ include/des/transition_proxy.hpp
@@ -39,15 +39,56 @@ class DESystem;
 
 class TransitionProxy {
 public:
+    /*! \brief TransitionProxy unique constructor
+     *
+     * Feed const data-members.
+     *
+     * @param aSysPtr Raw pointer to owner of the elem which will be proxied
+     * @param aLin Line of the element
+     * @param aCol Column of the element
+     */
     TransitionProxy(DESystem *const aSysPtr, cldes_size_t const &aLin,
                     cldes_size_t const &aCol);
+
+    /*! \brief Override operator "=" from TransitionProxy class
+     *
+     * Override operator "=" for tracking when the device graph from the
+     * related DESystem object is outdated.
+     *
+     * @param aTransitionValue graph_(lin_, col_) new value
+     */
     TransitionProxy &operator=(ScalarType aTransitionValue);
+
+    /*! \brief Override cast to ScalarType
+     *
+     * Operator used when element from graph_ is accessed, but its value is not
+     * changed.
+     */
     operator ScalarType();
 
 private:
+    /*! \brief Disabled default constructor
+     *
+     * There is no use for the default constructor.
+     */
     TransitionProxy();
+
+    /*! \brief Raw pointer to DESystem object
+     *
+     * Raw pointer to the owner of the proxied element.
+     */
     DESystem * sys_ptr_;
+
+    /*! \brief Element line.
+     *
+     * Line where the elem is located.
+     */
     cldes_size_t const lin_;
+
+    /*! \brief Column line.
+     *
+     * Column where the elem is located.
+     */
     cldes_size_t const col_;
 };
 
