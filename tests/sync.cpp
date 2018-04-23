a6f1cff5e935a3c4e566f1ede8b1e62efed333de
deam0nd <adrianomourao@protonmail.com>
2018-04-23 14:10:41 -0300

    Adding cldes::op::Synchronize test

---
+++ tests/sync.cpp
@@ -0,0 +1,72 @@
+/*
+ =========================================================================
+ This file is part of clDES
+
+ clDES: an OpenCL library for Discrete Event Systems computing.
+
+ clDES is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ clDES is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with clDES.  If not, see <http://www.gnu.org/licenses/>.
+
+ Copyright (c) 2018 - Adriano Mourao <adrianomourao@protonmail.com>
+                      madc0ww @ [https://github.com/madc0ww]
+
+ LacSED - Laboratório de Sistemas a Eventos Discretos
+ Universidade Federal de Minas Gerais
+
+ File: test/kernels.hpp
+ Description: Test cldes::op::Synchronize function, the parallel
+ composition implementation.
+ =========================================================================
+*/
+
+#include "cldes.hpp"
+
+int main() {
+    // Declare transitions: represented by prime numbers
+    const float a = 2.0f;
+    const float b = 3.0f;
+    const float g = 5.0f;
+
+    // Declare system G1
+    int const nstatesG1 = 3;
+
+    cldes::DESystem::StatesSet markedstatesG1;
+    markedstatesG1.insert(0);
+    markedstatesG1.insert(2);
+
+    int const initstateG1 = 0;
+    cldes::DESystem g1{nstatesG1, initstateG1, markedstatesG1};
+
+    g1(0, 0) = a;
+    g1(0, 2) = g;
+    g1(1, 0) = a;
+    g1(1, 1) = b;
+    g1(2, 1) = a * g;
+    g1(2, 2) = b;
+
+    // Declare system G2
+    int const nstatesG2 = 2;
+
+    cldes::DESystem::StatesSet markedstatesG2;
+    markedstatesG1.insert(1);
+
+    int const initstateG2 = 0;
+    cldes::DESystem g2{nstatesG2, initstateG2, markedstatesG2};
+
+    g2(0, 0) = b;
+    g2(0, 1) = a;
+    g2(1, 0) = b;
+    g2(1, 1) = a;
+
+    return 0;
+}
